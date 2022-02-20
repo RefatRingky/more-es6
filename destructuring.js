a55f62a1f010d428c4437188c1270675923f0cfb
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-20 13:02:27 +0600

    second commit

--- destructuring.js
+++ destructuring.js
@@ -1,3 +1,7 @@
 const fish = {id:15203016, address: 'chandpur',name:'Hilsa King',phone:'0175551567',dress:'silver'}
-
+const phone = fish.phone;
+const dress =  fish.price;
+const name = fish.name;
 console.log(fish.phone);
+console.log(fish.dress);
+console.log(fish.name);
