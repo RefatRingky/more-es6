c0c8ab1cf3773e633f25727d678cdcd368dacd68
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-20 13:14:24 +0600

    destructuring commit

--- destructuring.js
+++ destructuring.js
@@ -2,6 +2,28 @@ const fish = {id:15203016, address: 'chandpur',name:'Hilsa King',phone:'01755515
 const phone = fish.phone;
 const dress =  fish.price;
 const name = fish.name;
-console.log(fish.phone);
-console.log(fish.dress);
-console.log(fish.name);
+// console.log(fish.phone);
+// console.log(fish.dress);
+// console.log(fish.name);
+
+const company = {
+    name: 'GP',
+    ceo: { id: 1, name: 'ajmol', food: 'fuchka' },
+    web: {
+        work: 'website development',
+        employee: 22,
+        framework: 'react',
+        tech: {
+            first: 'html',
+            second: 'css',
+            third: 'js'
+        }
+    },
+};
+
+// const work = company.web.work;
+// const framework = company.web.framework;
+const { work, framework } = company.web;
+const { food } = company.ceo;
+const { second, third } = company.web.tech;
+console.log(work, framework, food);
